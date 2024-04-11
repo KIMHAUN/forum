4033175ca43c195f660e5669e76600f7784861df
KIMHAEUN <wha02068@gmail.com>
2024-04-11 15:56:29 +0900

    상세페이지 만들기

---
+++ app/list/page.js
@@ -0,0 +1,33 @@
+import { connectDB } from "@/util/database"
+import Link from 'next/link'
+import DetailLink from "./DetailLink"
+
+//export const dynamic = 'force-dynamic'
+export const revalidate = 60;
+
+export default async function List() {
+
+  const db = (await connectDB).db("forum")
+  let result = await db.collection('post').find().toArray()
+  console.log(result)
+
+  //await fetch('/URL', {cache: 'force-cache'})
+
+  let data = { }
+    return (
+        <div className="list-bg">
+          {
+            result.map((a, i)=> 
+              
+              
+                <div className="list-item" key={i}>
+                  <Link prefetch={false} href={`/detail/${a._id}`}><h4>{result[i].title}</h4></Link>
+                  <p>{result[i].content}</p>
+                  <DetailLink/>
+                </div>  
+              
+            )
+          }
+      </div>
+    )
+}
---
+++ app/list2/page.js
@@ -0,0 +1,31 @@
+import { connectDB } from "@/util/database"
+import Link from 'next/link'
+import DetailLink from "./DetailLink"
+
+//export const dynamic = 'force-dynamic'
+export const revalidate = 20;
+
+export default async function List() {
+
+  const db = (await connectDB).db("forum")
+  let result = await db.collection('post').find().toArray()
+  console.log(result)
+
+  let data = { }
+    return (
+        <div className="list-bg">
+          {
+            result.map((a, i)=> 
+              
+              
+                <div className="list-item" key={i}>
+                  <Link prefetch={false} href={`/detail/${a._id}`}><h4>{result[i].title}</h4></Link>
+                  <p>{result[i].content}</p>
+                  <DetailLink/>
+                </div>  
+              
+            )
+          }
+      </div>
+    )
+}
